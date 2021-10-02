c085aa385d70062acfd89059b3d12e900bbf147a
zura0z <zuramazmishvili@gmail.com>
2021-10-02 14:53:59 +0400

    deploy

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === "production") {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () =>
+app.listen(PORT, () =>
   console.log(
     `Server is up and running in ${process.env.NODE_ENV} mode on port ${PORT}`
   )

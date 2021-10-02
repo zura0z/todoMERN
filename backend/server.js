64cd05fab4f9828756e4d4b327660924f94f8083
zura0z <zuramazmishvili@gmail.com>
2021-10-02 14:16:11 +0400

    deploy

--- backend/server.js
+++ backend/server.js
@@ -21,14 +21,14 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "frontend", "build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
   );
 }
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(5000, () =>
   console.log(

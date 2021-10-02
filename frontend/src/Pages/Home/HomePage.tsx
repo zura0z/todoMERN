672a1caae793bb373eb00d642a51ee21750073ef
zura0z <zuramazmishvili@gmail.com>
2021-10-02 13:57:06 +0400

    Home Page

--- frontend/src/Pages/Home/HomePage.tsx
+++ frontend/src/Pages/Home/HomePage.tsx
@@ -1,12 +1,16 @@
+import BaseComponent from '../../Components/Commons/Base.Component';
+
 import illustration from '../../utils/illustration.svg';
 import './HomePage.css';
 
-const HomePage = () => {
-    return (
-        <main className="homePage-container">
-            <img src={illustration} className="illustration" />
-        </main>
-    );
-};
+class HomePage extends BaseComponent {
+    render() {
+        return (
+            <main className="homePage-container">
+                <img src={illustration} className="illustration" />
+            </main>
+        );
+    }
+}
 
 export default HomePage;
